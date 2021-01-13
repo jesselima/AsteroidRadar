395223305af7d6f0a868590c459fa6d286a22fd9
Jesse Lima <jesselima.tech@gmail.com>
2021-01-13 06:18:14 -0300

    Added: PictureOfTheDayDao

---
+++ app/src/main/java/com/udacity/asteroidradar/features/main/data/datasource/local/dao/PictureOfTheDayDao.kt
@@ -0,0 +1,25 @@
+package com.udacity.asteroidradar.features.main.data.datasource.local.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Query
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import com.udacity.asteroidradar.features.main.domain.entities.PictureOfDay
+
+@Dao
+interface PictureOfTheDayDao {
+
+    @Query("SELECT * FROM picture_of_the_day WHERE id = :date")
+    fun getLocalPictureOfTheDay(date: String): LiveData<PictureOfDay>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(pictureOfDay: PictureOfDay) : Long
+
+    @Update
+    fun update(pictureOfDay: PictureOfDay) : Int
+
+    @Query("DELETE FROM picture_of_the_day")
+    fun deleteAll() : Int
+}
