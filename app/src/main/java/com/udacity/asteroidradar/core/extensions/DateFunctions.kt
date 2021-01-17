425270f13634269798c208b4551560e43225eee1
Jesse Lima <jesselima.tech@gmail.com>
2021-01-17 16:22:47 -0300

    Changed: Improve getCurrentDate function.

--- app/src/main/java/com/udacity/asteroidradar/core/extensions/DateFunctions.kt
+++ app/src/main/java/com/udacity/asteroidradar/core/extensions/DateFunctions.kt
@@ -1,17 +1,17 @@
 package com.udacity.asteroidradar.core.extensions
 
-import java.util.Calendar
+import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.Date
 
 /**
  * Created by jesselima on 10/01/21.
  * This is a part of the project Asteroid Radar.
  */
 
+private const val DATE_DATABASE_FORMAT = "yyyy-MM-dd"
+
 fun getCurrentDate() : String {
-	val calendar = Calendar.getInstance(Locale.getDefault())
-	val currentYear = calendar.get(Calendar.YEAR)
-	val currentMonth = calendar.get(Calendar.MONTH)
-	val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
-	return "$currentYear-$currentMonth-$currentDay"
+	val formatter = SimpleDateFormat(DATE_DATABASE_FORMAT, Locale.getDefault())
+	return formatter.format(Date())
 }
