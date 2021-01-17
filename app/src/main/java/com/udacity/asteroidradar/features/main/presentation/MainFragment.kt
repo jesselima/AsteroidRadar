dd3c1f3cfd4fe83ef91b39af9fb3b402e3b69573
Jesse Lima <jesselima.tech@gmail.com>
2021-01-17 17:10:50 -0300

    Added: ViewModel Observers with logs

--- app/src/main/java/com/udacity/asteroidradar/features/main/presentation/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/features/main/presentation/MainFragment.kt
@@ -2,15 +2,17 @@ package com.udacity.asteroidradar.features.main.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.view.MenuInflater
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class MainFragment : Fragment() {
 
@@ -26,6 +28,23 @@ class MainFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.pictureOfTheDay.observe(viewLifecycleOwner, {
+            it?.let { result ->
+                Timber.d("PictureOfTheDay -->>> $result")
+                textView.text = result.title
+            }
+        })
+        viewModel.asteroidFeed.observe(viewLifecycleOwner, {
+            it?.let { result ->
+                Timber.d("SIZE -->>> ${result.size}")
+                Timber.d("Data -->>> $result")
+            }
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
