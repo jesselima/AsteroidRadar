d7a2bced2820c2a8a39492eaa12a6d5d3f074866
Jesse Lima <jesselima.tech@gmail.com>
2021-02-07 21:15:41 -0300

    Add buildConfigField to build.gradle file to handle API keys.

--- app/src/main/java/com/udacity/asteroidradar/features/main/data/datasource/remote/api/AsteroidRadarService.kt
+++ app/src/main/java/com/udacity/asteroidradar/features/main/data/datasource/remote/api/AsteroidRadarService.kt
@@ -1,6 +1,6 @@
 package com.udacity.asteroidradar.features.main.data.datasource.remote.api
 
-import com.udacity.asteroidradar.core.api.Environment
+import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.database.asteroids.models.AsteroidsFeedResponse
 import com.udacity.asteroidradar.features.main.data.models.PictureOfDayResponse
 import retrofit2.http.GET
@@ -15,25 +15,25 @@ interface AsteroidRadarService {
     @GET("neo/rest/v1/feed")
     suspend fun getRemoteAsteroidFeed(
         @Query("start_date") startDate: String,
-        @Query("api_key") apiKey: String = Environment.getApiKey()
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ) : AsteroidsFeedResponse?
 
     @GET("planetary/apod")
     suspend fun getRemotePictureOfTheDay(
-        @Query("api_key") apiKey: String = Environment.getApiKey()
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ) : PictureOfDayResponse?
 
     @GET("planetary/apod")
     suspend fun getRemotePictureOfTheDayByDate(
         @Query("date") date: String,
-        @Query("api_key") apiKey: String = Environment.getApiKey()
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ) : PictureOfDayResponse?
 
     @GET("planetary/apod")
     suspend fun getRemotePictureOfTheLastSevenDays(
         @Query("start_date") startDate: String,
         @Query("end_date") endDate: String,
-        @Query("api_key") apiKey: String = Environment.getApiKey()
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ) : List<PictureOfDayResponse>?
 
 }
