14b9a929e7855093e4e700d4646ea5c0fa0e88f1
Jesse Lima <jesselima.tech@gmail.com>
2021-02-23 22:53:27 -0300

    Changed: Pictures gallery - Replace toast with lottie animation when no results found.

--- app/src/main/java/com/udacity/asteroidradar/features/picturesfavoritesgallery/PicturesGalleryFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/features/picturesfavoritesgallery/PicturesGalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.udacity.asteroidradar.R
@@ -62,13 +63,9 @@ class PicturesGalleryFragment : Fragment() {
 		viewModel.picturesOfDay.observe(viewLifecycleOwner, { favoritePictures ->
 			favoritePictures.whenNotNull {
 				picturesGalleryAdapter.submitList(this)
-				if (favoritePictures?.isEmpty() == true) {
-					showAppToast(
-						getString(R.string.no_favorite_pictures_found_try_to_add_some),
-						ToastType.INFO,
-						toastDuration = Toast.LENGTH_LONG
-					)
-				}
+				animatedNoFavoritesPicturesFound.isVisible = favoritePictures?.isEmpty() == true
+				animatedNoFavoritesTextBalonMessage.isVisible = favoritePictures?.isEmpty() == true
+				noFavoritesFoundEmptySpaceBackground.isVisible = favoritePictures?.isEmpty() == true
 			}
 		})
 	}
